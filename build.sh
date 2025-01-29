80999cb96c7677239802a917dcdd979aed6d4114
PrototypeActual <18661121+PrototypeActual@users.noreply.github.com>
2025-01-29 01:19:44 -0500

    Switch default tag to stable name and commands to install vscodium

--- build.sh
+++ build.sh
@@ -4,22 +4,27 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+## Packages can be installed from any enabled yum repo on the image.
+## RPMfusion repos are available by default in ublue main images
+## List of rpmfusion packages can be found here:
+## https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-##This will install rpmfusion free/nonfree repos
-# dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+## This will install rpmfusion free/nonfree repos
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# this installs a package from fedora repos
-dnf install -y tmux
+## this installs a package from fedora repos
+# dnf install -y tmux
 
-# Use a COPR Example:
-#
+## VSCodium install
+rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+dnf -y install codium
+
+## Use a COPR Example:
+##
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
+## Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File

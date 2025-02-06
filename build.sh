4e45b095a240ff9ad52ea2e19d4d4f5de25f8090
Cristian Torres <18661121+PrototypeActual@users.noreply.github.com>
2025-02-06 15:40:00 -0500

    Added discord

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-
 ## VSCodium install
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee -a /etc/yum.repos.d/vscodium.repo
-dnf -y install codium
+dnf -y install codium discord
 
 ## Use a COPR Example:
 ##

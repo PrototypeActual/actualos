7c98991f110f7c31933682b6689f7eef8fe3e9d8
Cristian Torres <18661121+PrototypeActual@users.noreply.github.com>
2025-02-08 18:56:45 -0500

    Removed update and auto remove command

--- build.sh
+++ build.sh
@@ -20,8 +20,6 @@ rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/mast
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee -a /etc/yum.repos.d/vscodium.repo
 
 ## Install programs with DNF
-dnf -y update
-dnf -y autoremove
 dnf -y install codium discord
 
 # dnf -y install codium discord steam

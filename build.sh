cc28c2042992a460212dabddf9237a89010b1933
PrototypeActual <18661121+PrototypeActual@users.noreply.github.com>
2025-02-08 16:34:30 -0500

    Added notes

--- build.sh
+++ build.sh
@@ -15,9 +15,11 @@ dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-
 ## this installs a package from fedora repos
 # dnf install -y tmux
 
-## VSCodium install
+## VSCodium preinstall tasks
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee -a /etc/yum.repos.d/vscodium.repo
+
+## Install programs with DNF
 dnf -y install codium discord steam
 
 ## Use a COPR Example:

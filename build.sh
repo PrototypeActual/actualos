01d482c93ac016963fa53443490b7889d440d591
PrototypeActual <18661121+PrototypeActual@users.noreply.github.com>
2025-03-08 02:27:20 -0500

    Removed steam

--- build.sh
+++ build.sh
@@ -22,11 +22,11 @@ rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/mast
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee -a /etc/yum.repos.d/vscodium.repo
 
 ## Install programs with DNF
-# dnf -y install codium discord
+# dnf5 -y install codium discord steam python3-tkinter
+
+dnf5 -y install codium discord python3-tkinter
 
 # rpm-ostree install codium discord python3-tkinter
-
-dnf5 -y install codium discord steam python3-tkinter
 
 ## Use a COPR Example:
 ##

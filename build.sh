636c03324d276bf75b74f7edd71672c2797cb91c
PrototypeActual <18661121+PrototypeActual@users.noreply.github.com>
2025-03-08 04:33:49 -0500

    Test to install steam

--- build.sh
+++ build.sh
@@ -12,11 +12,6 @@ set -ouex pipefail
 ## This will install rpmfusion free/nonfree repos
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-## this installs a package from fedora repos
-# dnf install -y tmux
-
 ## VSCodium preinstall tasks
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee -a /etc/yum.repos.d/vscodium.repo
@@ -26,7 +21,7 @@ printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbase
 
 dnf5 -y install codium discord python3-tkinter
 
-rpm-ostree install steam
+dnf5 install -y steam --enablerepo=rpmfusion-nonfree-steam
 
 # rpm-ostree install codium discord python3-tkinter
 

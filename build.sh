03214a60a035f95d8e56859488e39a079b26172e
PrototypeActual <18661121+PrototypeActual@users.noreply.github.com>
2025-03-12 01:21:51 -0400

    Swapped tailscale repo install

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/mast
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee -a /etc/yum.repos.d/vscodium.repo
 
 ## Tailscale preinstall tasks
-dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo -O /etc/yum.repos.d/tailscale.repo
 
 ## Install programs with DNF
 dnf5 -y install virt-manager tailscale codium discord python3-tkinter

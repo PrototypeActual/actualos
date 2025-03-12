7450250de605f8b106fa511c31eecbc07f949d7c
PrototypeActual <18661121+PrototypeActual@users.noreply.github.com>
2025-03-12 01:41:47 -0400

    Removed Lutris and added pull app test

--- build.sh
+++ build.sh
@@ -17,7 +17,9 @@ rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/mast
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee -a /etc/yum.repos.d/vscodium.repo
 
 ## Install programs with DNF
-dnf5 -y install virt-manager codium discord python3-tkinter
+dnf5 -y install virt-manager codium discord
+
+dnf5 -y remove lutris
 
 # dnf5 -y install codium discord python3-tkinter
 

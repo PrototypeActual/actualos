1d7f5889173f1fc894e718287a903a13b3d5f1b4
Cristian Torres <18661121+PrototypeActual@users.noreply.github.com>
2025-07-11 01:55:22 -0400

    Removed codium and added python package

--- build.sh
+++ build.sh
@@ -12,10 +12,6 @@ set -ouex pipefail
 ## This will install rpmfusion free/nonfree repos
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-## VSCodium preinstall tasks
-# rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-# printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee -a /etc/yum.repos.d/vscodium.repo
-
 ## VSCode preinstall tasks
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
@@ -24,11 +20,9 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo -O /etc/yum.repos.d/tailscale.repo
 
 ## Install programs with DNF
-dnf5 -y install virt-manager tailscale code
+dnf5 -y install virt-manager tailscale code python3-tkinter
 
 dnf5 -y remove discover-overlay
-
-# dnf5 -y install virt-manager tailscale codium python3-tkinter
 
 ## Use a COPR Example:
 # dnf5 -y copr enable ublue-os/staging

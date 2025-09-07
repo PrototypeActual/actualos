9a950900e2f199991780b9ba4bbb3e013291a4b4
Cristian Torres <18661121+PrototypeActual@users.noreply.github.com>
2025-09-07 16:25:32 -0400

    Added Discord attempt

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 ## https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 ## This will install rpmfusion free/nonfree repos
-dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 ## VSCode preinstall tasks
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -20,7 +21,7 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo -O /etc/yum.repos.d/tailscale.repo
 
 # Install programs with DNF
-dnf5 -y install virt-manager tailscale code python3-tkinter
+dnf5 -y install virt-manager tailscale code discord python3-tkinter
 
 ## Use a COPR Example:
 # dnf5 -y copr enable ublue-os/staging

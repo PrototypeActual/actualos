9626b47b9ec92a89e0ddbe2f3c9679ce5ded1643
Cristian Torres <18661121+PrototypeActual@users.noreply.github.com>
2025-09-07 16:56:49 -0400

    Search for Discord

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo -O /etc/yum.repos.d/tailscale.repo
 
 # Install programs with DNF
+dnf5 search discord
 dnf5 -y install virt-manager code python3-tkinter
 
 ## Use a COPR Example:

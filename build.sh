bade71b241be095e6667f0b261fa35bfe06ffbf5
Cristian Torres <18661121+PrototypeActual@users.noreply.github.com>
2025-09-07 17:06:29 -0400

    Removed tailscale and dnf search

--- build.sh
+++ build.sh
@@ -13,11 +13,7 @@ set -ouex pipefail
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-## Tailscale preinstall tasks
-wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo -O /etc/yum.repos.d/tailscale.repo
-
 # Install programs with DNF
-dnf5 search discord
 dnf5 -y install virt-manager code python3-tkinter
 
 ## Use a COPR Example:
